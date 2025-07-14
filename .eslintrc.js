b8db089ee8ea6b196c92fd9e2048c0772f12508d
Batyr <bshekish@gmail.com>
2025-07-14 15:10:23 +0500

    chore: initialize project scaffold with CRA, ESLint, Prettier, and routing

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', 
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'max-lines-per-function': ['warn', { max: 40 }],
+    'no-magic-numbers': ['warn', { ignore: [0, 1], enforceConst: true }],
+    'react/react-in-jsx-scope': 'off', 
+  },
+};
